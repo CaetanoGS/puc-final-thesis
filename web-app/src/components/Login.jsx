761c157e06b2351539416e6a60b8b0415e672241
Gustavo Caetano <CaetanooG@hotmail.com>
2023-02-05 13:58:07 +0100

    feat(core): Create login screen
    Create the login screen style based on the following created template on Figma:
    https://www.figma.com/file/JqeiC3piYl6tq0smsumpnv/puc-projeto-final?node-id=1%3A232&t=Hv8hhrr27mhhnrNL-1

---
+++ web-app/src/components/Login.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import loginImg from "../assets/login.png";
+
+export default function() {
+    return(
+        <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+            <div className="hidden sm:block">
+                <img className="w-full h-full object-cover" src={loginImg} alt="" />
+            </div>
+
+            <div className="flex flex-col justify-center">
+                <form action="" className="max-w-[400px] w-full mx-auto p-8 px-8 rounded-lg">
+                    <div className="flex flex-col py-2 text-gray-400">
+                        <input type="text" placeholder="Username" className="rounded-lg bg-gray-200 mt-2 p-2 focus:bg-gray-300 focus:outline-none"/>
+                    </div>
+                    <div className="flex flex-col py-2 text-gray-400">
+                        <input type="password" placeholder="Password" className="rounded-lg bg-gray-200 mt-2 p-2 focus:bg-gray-300 focus:outline-none"/>
+                    </div>
+
+                    <button className="w-full my-5 py-2 bg-[#5678CE] rounded-lg text-white">Login</button>
+
+                    <div className="flex justify-center text-gray-400 py-2 text-sm">
+                        <p>Don't have an account yet ? Register here</p>
+
+                    </div>
+                </form>
+            </div>
+            
+        </div>
+    )
+}
